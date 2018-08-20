1e5537b9d5cc19891c90bbfa276b4f42e3f9b307
Tayguara Acioli <tayguaracioli@gmail.com>
2018-08-20 15:12:55 -0700

    Add files via upload

---
+++ public/js/directives/minhas-diretivas.js
@@ -0,0 +1,29 @@
+angular.module('minhasDiretivas', [])
+.directive('meuPainel', function() {
+
+	var ddo = {};
+
+	ddo.restric = "AE";
+	ddo.scope = {
+		titulo : '@',
+		url: '@'
+	};
+	ddo.templateUrl = 'js/directives/meu-painel.html';
+
+	return ddo;
+
+})
+.directive('minhaFoto', function(){
+
+	var ddo = {};
+
+	ddo.restric = "AE";
+	ddo.scope = {
+		titulo : '@',
+		url: '@'
+	};
+	ddo.template = '<img class="img-responsive center-block" ng-src="{{url}}" alt="{{titulo}}">';
+
+	return ddo;
+
+});
