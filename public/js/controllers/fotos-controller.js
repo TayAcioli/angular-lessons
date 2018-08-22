81c78d6ec40c4f4dc4e2ee50cdff413a0421a60f
Tayguara Acioli <tayguaracioli@gmail.com>
2018-08-22 00:36:19 -0700

    Update fotos-controller.js

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,14 +1,28 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http) {
 
-	$scope.fotos = [];
-	$scope.filtro = "";
+    $scope.fotos = [];
+    $scope.filtro = '';
+    $scope.mensagem = '';
 
-	$http.get('v1/fotos')
-	.success(function (fotos) {
-		$scope.fotos = fotos;
-	})
-	.error(function (erro){
-		console.log(erro);
-	});
+    $http.get('/v1/fotos')
+    .success(function(retorno) {
+        $scope.fotos = retorno;
+    })
+    .error(function(erro) {
+        console.log(erro)
+    });
 
+    $scope.remover = function(foto) {
+
+    	$http.delete('v1/fotos/' + foto._id)
+    	.success(function(){
+    		var indiceFoto = $scope.fotos.indexOf(foto);
+    		$scope.fotos.splice(indiceFoto,1);
+    		$scope.mensagem = foto.titulo + ' was successfully removed!';
+    	})
+    	.error(function(erro){
+    		$scope.mensagem = 'There was an error during the removal and the photo ' + foto.titulo + ' was not removed.';
+    		console.log(erro);
+    	});
+    };
 });
