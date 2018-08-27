c678269dd8ff69dd546b2bdc60a178e9ac4f7f2c
Tayguara Acioli <tayguaracioli@gmail.com>
2018-08-27 14:14:31 -0700

    Update minhas-diretivas.js

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -1,36 +1,78 @@
-angular.module('alurapic').controller('FotoController', function($scope, cadastroDeFotos, recursoFoto, $routeParams){
+angular.module('minhasDiretivas', [])
+.directive('meuPainel', function() {
 
-	$scope.foto = {};
-	$scope.mensagem = '';
+    var ddo = {};
 
-	if($routeParams.fotoId){
-		
-		recursoFoto.get({fotoId : $routeParams.fotoId}, function(foto){
-			$scope.foto = foto;
-		}, function(erro){
-			console.log(erro);
-			$scope.mensagem = 'An error occured during the transaction!';
-		});
-		
-	}
+    ddo.restrict = "AE";
+    ddo.transclude = true;
 
-	$scope.submeter = function() {
+    ddo.scope = {
+        titulo: '@'
+    };
 
-		if(!$scope.formulario.$valid){
-			return;
-		}
+   ddo.templateUrl = 'js/directives/meu-painel.html';        
 
-		cadastroDeFotos.cadastrar($scope.foto)
-		.then(function(dados){
-			$scope.mensagem = dados.mensagem;
-			if(dados.inclusao){
-				$scope.foto = {};
-			}
-		})
-		.catch(function(dados){
-			$scope.mensagem = dados.mensagem;
-		});
-		
+    return ddo;
+})
+.directive('minhaFoto', function(){
+
+	var ddo = {};
+
+	ddo.restric = "AE";
+	ddo.scope = {
+		titulo : '@',
+		url: '@'
 	};
+
+	ddo.template = '<img class="img-responsive center-block" ng-src="{{url}}" alt="{{titulo}}">';
+
+	return ddo;
+
+})
+.directive('meuBotaoPerigo', function(){
+
+	var ddo  = {};
+
+	ddo.restric = "E";
+	ddo.scope = {
+		nome: '@',
+		acao: '&'
+	};
+
+	ddo.template = '<button class="btn btn-danger btn-block" ng-click="acao(foto)">{{nome}}</button>';
+
+	return ddo;
+
+})
+.directive('meuFocus', function(){
+
+	var ddo = {};
+
+	ddo.restric = "A";
+
+	ddo.link = function(scope, element){
+		scope.$on('fotoCadastrada', function(){
+			element[0].focus();
+		});
+	};
+
+	return ddo;
+
+})
+.directive('meusTitulos', function(){
+
+	var ddo = {};
+
+	ddo.restric = "E";
+	ddo.template = '<ul><li ng-repeat="titulo in titulos">{{titulo}}</li></ul>';
+	ddo.controller = function($scope, recursoFoto) {
+        recursoFoto.query(function(fotos) {
+            $scope.titulos = fotos.map(function(foto) {
+                return foto.titulo;
+            });    
+        });
+    };
+
+	return ddo;
 
 });
